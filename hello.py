4c947a498ed94ce0de1e67610d00b69514287449
oishi <oishi@oishi-no-iMac.local>
2018-10-13 16:33:48 +0900

    1st commit

---
+++ hello.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# AquaMagicのデータファイル(.bin)を読みこんで、画像(.png)と海底深度(.csv)を出力する
+#
+# 引数
+#   第一引数:input_filename AquaMagicのデータファイル(.bin)のパス
+#   第二引数:output_folder  画像と海底深度の出力ディレクトリ。省略時は、binと同じディレクトリに出力される。
+# 
+# 作成日：2018/10/08
+# 作成者：大石卓司
+
+
+
+# 引数処理
+def get_args():
+    parser = argparse.ArgumentParser(description='Convert from AquaMagic data to PNG file')
+
+    parser.add_argument("input_filename", help="File path where AquaMagic data is located", nargs=None)
+    parser.add_argument("output_folder", help="Derectory path where you want to save PNG & CSV files", nargs='?',default=False)
+
+    args = parser.parse_args()
+    return(args)
+
+# メイン関数
+def main():
+    print('hello')
+    
+
+if __name__ == '__main__':
+    main()
